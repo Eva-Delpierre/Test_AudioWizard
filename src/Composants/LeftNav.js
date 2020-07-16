56ebeb3aa769ab9ed71a4ccdd3f653ffaaf577e2
Eva Delpierre <eva.delpierre@gmail.com>
2020-07-16 09:40:13 +0200

    MAJ leftNav

--- src/Composants/LeftNav.js
+++ src/Composants/LeftNav.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {Nav} from 'react-bootstrap';
 
+import { UserContext } from './UserContext';
+ 
+
 const LeftNav = () => {
+    const user = useContext(UserContext);
+
+    if(user.nomOk === true && user.sexeOk === true) {
+        return (
+        <div className="sidebar"> 
+            <Nav defaultActiveKey="/home" className="flex-column">
+                <Nav.Link href="/" className="blocMenu">1<sup>ère</sup> visite</Nav.Link>
+                <Nav.Link href="/" className="blocMenu">Adaptation</Nav.Link>
+                <Nav.Link href="/" className="blocMenu">Patients en attente</Nav.Link>
+                <Nav.Link href="/" className="blocMenu">Patients appareillés</Nav.Link>
+            </Nav>
+
+            <Nav defaultActiveKey="/home" className="flex-column">
+                <Nav.Link href="/" className="blocMenu">Maintenance</Nav.Link>
+                <Nav.Link href="/" className="blocMenu">Statistiques</Nav.Link>
+            </Nav>
+        </div>)
+      }
     return (
         <div className="sidebar"> 
                 <Nav defaultActiveKey="/home" className="flex-column">
                     <Nav.Link href="/" className="blocMenu">1<sup>ère</sup> visite</Nav.Link>
                     <Nav.Link href="/" className="blocMenu">Adaptation</Nav.Link>
                     <Nav.Link href="/" className="blocMenu">Patients en attente</Nav.Link>
-                    <Nav.Link href="/" className="blocMenu">Patients appareillés</Nav.Link>
                 </Nav>
 
                 <Nav defaultActiveKey="/home" className="flex-column">
